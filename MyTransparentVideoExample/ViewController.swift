ca963f10bd2900fa53bd3fdb6183975bb1777ae1
Quentin Fasquel <quentin.fasquel@gmail.com>
2017-10-27 08:15:39 +0200

    Initial Commit

---
+++ MyTransparentVideoExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTransparentVideoExample
+//
+//  Created by Quentin on 27/10/2017.
+//  Copyright © 2017 Quentin Fasquel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
