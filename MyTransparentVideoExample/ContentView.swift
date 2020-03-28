94c56aa269a7cf15cd5000fd962193af94468874
Quentin Fasquel <quentin.fasquel@gmail.com>
2020-03-27 17:03:23 -0700

    Setting up Swift-UI base app

---
+++ MyTransparentVideoExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTransparentVideoExample
+//
+//  Created by Quentin Fasquel on 25/03/2020.
+//  Copyright © 2020 Quentin Fasquel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color(.white)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
